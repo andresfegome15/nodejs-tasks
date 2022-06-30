805a4807a64973e2f5b8b2757848cff4e76dbe7b
andresfegome <andresfegome15@gmail.com>
2022-06-29 23:52:10 -0500

    secund commit

--- utils/db.utils.js
+++ utils/db.utils.js
@@ -1,12 +1,15 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
 const db = new Sequelize({
   dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  port: 5432,
-  password: "acuario1",
-  database: "entregables2",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB,
   logging: false,
 });
 
